c231a029131553df17c6e03a38299c1341ea0e62
Leonardo Santana <82040303+leosantanadev@users.noreply.github.com>
2022-02-11 21:01:00 -0300

    my layout loading page

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,40 +1,40 @@
-import { Box, Heading, Flex, Progress } from '@chakra-ui/react';
-
-export function Loading(): JSX.Element {
-  return (
-    <Flex
-      justifyContent="center"
-      alignItems="center"
-      h="100vh"
-      flexDir="column"
-    >
-      <Box>
-        <Heading>Carregando aplicação...</Heading>
-        <Progress
-          mt={4}
-          size="xs"
-          isIndeterminate
-          bgColor="transparent"
-          colorScheme="orange"
-        />
-      </Box>
-    </Flex>
-  );
-}
-
-// MY LAYOUT
-
-// import { Heading, Center, Stack, Spinner } from '@chakra-ui/react';
+// import { Box, Heading, Flex, Progress } from '@chakra-ui/react';
 
 // export function Loading(): JSX.Element {
 //   return (
-//     <Center width="100%" height="100%" minHeight="75vh">
-//       <Stack direction="column" spacing={5}>
-//         <Heading fontSize={22}>Carregando aplicação...</Heading>
-//         <Center>
-//           <Spinner size="lg" />
-//         </Center>
-//       </Stack>
-//     </Center>
+//     <Flex
+//       justifyContent="center"
+//       alignItems="center"
+//       h="100vh"
+//       flexDir="column"
+//     >
+//       <Box>
+//         <Heading>Carregando aplicação...</Heading>
+//         <Progress
+//           mt={4}
+//           size="xs"
+//           isIndeterminate
+//           bgColor="transparent"
+//           colorScheme="orange"
+//         />
+//       </Box>
+//     </Flex>
 //   );
 // }
+
+// MY LAYOUT
+
+import { Heading, Center, Stack, Spinner } from '@chakra-ui/react';
+
+export function Loading(): JSX.Element {
+  return (
+    <Center width="100%" height="100%" minHeight="75vh">
+      <Stack direction="column" spacing={5}>
+        <Heading fontSize={22}>Carregando aplicação...</Heading>
+        <Center>
+          <Spinner size="lg" />
+        </Center>
+      </Stack>
+    </Center>
+  );
+}
